5fb5bbe1313ab77d1867979be2b2c731fed993ca
Eve Ali <ibrahim.ali94@myhunter.cuny.edu>
2017-09-10 23:50:03 -0400

    First assignment

---
+++ 01/bondify.py
@@ -0,0 +1,5 @@
+badbond = input()
+badbond = badbond.partition(" ")
+first = badbond[0]
+last = badbond[2]
+print(last+", "+first+" "+last+ ".")
---
+++ 01/nameify.py
@@ -0,0 +1,5 @@
+lower = input()
+lower = lower.partition(" ")
+namea = lower[0]
+nameb = lower[2]
+print(namea.capitalize()+" "+nameb.capitalize())
