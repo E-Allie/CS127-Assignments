62d711499c5d18f4aa682c1416e105eee95889b4
Eve Ali <ibrahim.ali94@myhunter.cuny.edu>
2017-10-19 12:30:01 -0400

    Implemented Hackerrank Testing

--- 08/ladybug.py
+++ 08/ladybug.py
@@ -12,11 +12,11 @@ def buglist(b):
             if freq(bug,board)<=1:
                 return("NO")
     if "_" not in board:
-        if happy(board):
+        if unhappy(board):
             return("NO")
     return("YES")
 
-def happy(bugs):
+def unhappy(bugs):
     if len(bugs)==2:
         if bugs[0]!=bugs[1]:
             return True
@@ -29,9 +29,18 @@ def happy(bugs):
             return True
 
 
-games=open("bugsimport").read().splitlines()
+"""games=open("bugsimport").read().splitlines()
 for i in games:
     z=i.replace("_","")
     if z.isalpha()==True:
         print(buglist(i))
+"""
+
+g = int(input().strip())
+for a0 in range(g):
+    n = int(input().strip())
+    b = input().strip()
+    print(buglist(b))
+    
+#Hackerrank tested, hacker approved!
 
