41fa41d6c211bdb92003554a555f3eadc8949d77
Eve Ali <ibrahim.ali94@myhunter.cuny.edu>
2017-10-19 02:53:55 -0400

    Lab 08

---
+++ 08/applesandoranges.py
@@ -0,0 +1,26 @@
+def trees(hstart,hend,atree,btree,apples,bapples,dapp,dbapp):
+    happ=0
+    hbapp=0
+    
+    for i in range(apples):
+        if hstart<=atree+dapp[i]<=hend:
+            happ+=1
+    for i in range(bapples):
+        if hstart<=btree+dbapp[i]<=hend:
+            hbapp+=1
+    return(happ,hbapp)
+
+fruits=open("fruitsimport").read().splitlines()
+s,t=fruits[0].split(" ")
+a,b=fruits[1].split(" ")
+m,n=fruits[2].split(" ")
+da=[]
+do=[]
+for i in fruits[3].split(" "):
+    da.append(int(i))
+for i in fruits[4].split(" "):
+    do.append(int(i))
+
+s,t,a,b,m,n=int(s),int(t),int(a),int(b),int(m),int(n)
+
+print(trees(s,t,a,b,m,n,da,do))
---
+++ 08/ladybug.py
@@ -0,0 +1,34 @@
+def freq(num,list):
+    r=0
+    for x in list:
+        if x==num:
+            r+=1
+    return(r)
+
+def buglist(b):
+    board=list(b)
+    for i, bug in enumerate(board):
+        if bug.isalpha()==True:
+            if freq(bug,board)<=1:
+                return("NO")
+    if "_" not in board:
+        x=happy(board)
+        if x==1:
+            return("NO")
+    return("YES")
+
+def happy(bugs):
+    for i, bug in enumerate(bugs):
+        try:
+            if bug!=bugs[i+1] and bug!=bugs[i-1]:
+                return(1)
+        except:
+            pass
+
+
+games=open("bugsimport").read().splitlines()
+for i in games:
+    z=i.replace("_","")
+    if z.isalpha()==True:
+        print(buglist(i))
+
